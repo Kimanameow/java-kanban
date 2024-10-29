0d98eecc3e92c1f7ab9a9264fc55de429f8931d7
Anasasiia Novikova <kimana_meow@bk.ru>
2024-10-29 19:33:12 +0300

    Codestyle

--- src/server/BaseHttpHandler.java
+++ src/server/BaseHttpHandler.java
@@ -4,9 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import history.HistoryManager;
 import taskmanager.TaskManager;
-import tasks.Epic;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
